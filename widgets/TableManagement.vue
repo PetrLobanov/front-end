e49bd299e192672e638b503f4f7221c84722588c
Vladimir Sartakov <vov.sartakov@mail.ru>
2024-08-13 19:00:42 +0300

    Add SASS and PUG

--- widgets/TableManagement.vue
+++ widgets/TableManagement.vue
@@ -1,50 +1,39 @@
-<template>
-    <div class="table-filter flex">
-        <div class="table-filler__item flex">
-            <img class="table-filler__item--img" src="/icons/closed-eye.svg" alt="">
-            <span>Спрятать поля</span>
-        </div>
-        <div class="table-filler__item flex">
-            <img class="table-filler__item--img" src="/icons/table.svg" alt="">
-            <span>табличный вид</span>
-            <img class="table-filler__item--img-help" src="/icons/table-bird.svg" alt="">
-        </div>
-        <div class="table-filler__item flex">
-            <img class="table-filler__item--img" src="/icons/filter.svg" alt="">
-            <span>фильтры</span>
-        </div>
-        <div class="table-filler__item flex">
-            <img class="table-filler__item--img" src="/icons/group.svg" alt="">
-            <span>сгруппировать</span>
-        </div>
-        <div class="table-filler__item flex">
-            <img class="table-filler__item--img" src="/icons/sort.svg" alt="">
-            <span>Сортировка</span>
-        </div>
-    </div>
+<template lang="pug">
+.table-filter.flex
+  .table-filler__item.flex
+    img.table-filler__item--img(src="/icons/closed-eye.svg" alt="")
+    span Спрятать поля
+  .table-filler__item.flex
+    img.table-filler__item--img(src="/icons/table.svg" alt="")
+    span Табличный вид
+    img.table-filler__item--img-help(src="/icons/table-bird.svg" alt="")
+  .table-filler__item.flex
+    img.table-filler__item--img(src="/icons/filter.svg" alt="")
+    span Фильтры
+  .table-filler__item.flex
+    img.table-filler__item--img(src="/icons/group.svg" alt="")
+    span Сгруппировать
+  .table-filler__item.flex
+    img.table-filler__item--img(src="/icons/sort.svg" alt="")
+    span Сортировка
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style scoped>
-    .table-filter {
-        padding-bottom: 26px;
-    }
-    .table-filler__item {
-        padding-left: 16px;
-        padding-right: 16px;
-        cursor: pointer;
-    }
-    .table-filler__item span {
-        font-weight: 500;
-        text-transform: uppercase;
-        font-size: 14px;
-    }
-    .table-filler__item  .table-filler__item--img {
-        padding-right: 8px;
-    }
-    .table-filler__item--img-help {
-        padding-left: 8px;
-    }
+<style lang="sass" scoped>
+.table-filter
+  padding-bottom: 26px
+  &__item
+    padding-left: 16px
+    padding-right: 16px
+    cursor: pointer
+  &__item span
+    font-weight: 500
+    text-transform: uppercase
+    font-size: 14px
+  &__item--img
+    padding-right: 8px
+  &__item--img-help
+    padding-left: 8px
 </style>
